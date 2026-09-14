98bc14779a8db3b7150b185d068701ed558fc1e4
kalab <kalabb69@gmail.com>
2026-09-14 21:19:54 +0300

    initial project structure with backend skeleton

---
+++ backend/CourseVault/src/main/java/com/example/coursevault/CourseVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseVaultApplication.class, args);
+    }
+
+}
---
+++ backend/CourseVault/src/test/java/com/example/coursevault/CourseVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
